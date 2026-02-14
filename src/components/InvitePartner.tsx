d53deb8f2952d8261cb45f38e503790a04864d92
D3Portillo <d3portillo@gmail.com>
2026-02-14 22:45:58 +0100

    feat: force login

--- src/components/InvitePartner.tsx
+++ src/components/InvitePartner.tsx
@@ -1,16 +1,22 @@
 "use client"
 
+import { useAuth } from "@/lib/wallet"
 import { useModalPartnerSync } from "./ModalPartnerSync"
 
 import { BsEnvelopeOpenHeartFill } from "react-icons/bs"
 import { FaChevronRight } from "react-icons/fa"
 
 export function InvitePartner() {
+  const { login, isConnected } = useAuth()
   const [, setIsOpen] = useModalPartnerSync()
 
   return (
     <button
-      onClick={() => setIsOpen(true)}
+      onClick={() => {
+        if (isConnected) return setIsOpen(true)
+        // Show wallet flow when not connected
+        login()
+      }}
       className="flex w-full items-center justify-between rounded-2xl bg-linear-to-br from-pink-300/50 border border-white/10 to-wb-violet/50 px-4 py-3 text-white"
     >
       <BsEnvelopeOpenHeartFill className="text-2xl" />
